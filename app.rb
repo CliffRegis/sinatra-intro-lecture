14fb1a1c81d902c7432259de236b5c91da562844
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-08-10 14:44:12 -0400

    starting state

--- app.rb
+++ app.rb
@@ -1,19 +1,5 @@
 require 'pry'
 class App < Sinatra::Base
-  get '/' do 
-    # "Hi and welcome to my incredibly advanced web application"
-    erb :index
-  end
+  
 
-  get '/home' do 
-    erb :home
-  end
-
-  get '/about' do 
-    erb :about
-  end
-
-  get '/users/:id' do 
-    binding.pry
-  end
 end
